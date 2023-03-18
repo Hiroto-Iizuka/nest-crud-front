0697326a6afe268f5571d697cfd82593520f1f46
Hiroto-Iizuka <oroxith@gmail.com>
2023-03-18 13:57:18 +0900

    feat: JWTリダイレクト

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,24 @@
 import { NextPage } from 'next'
-import React from 'react'
+import { UserInfo } from '../components/UserInfo'
+import { useQueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { Button } from '@mantine/core'
 
 const Dashboard: NextPage = () => {
+  const router = useRouter()
+  const queryClient = useQueryClient()
+  const signout = async () => {
+    queryClient.removeQueries(['user'])
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signout`)
+    router.push('/')
+  }
   return (
-    <div>Top Page</div>
+    <QueryClientProvider client={queryClient}>
+      <Button color="cyan" onClick={signout}>SignOut</Button>
+      CurrentUser: <UserInfo />
+    </QueryClientProvider>
   )
 }
+
+export default Dashboard
